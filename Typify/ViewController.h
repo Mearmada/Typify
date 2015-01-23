21c11762f9137fb44c7b012b9811b97e224be7de
John Heaton <jheaton.dev@gmail.com>
2015-01-23 16:31:06 -0500

    Initial Commit

---
+++ Typify/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Typify
+//
+//  Created by John Heaton on 1/23/15.
+//  Copyright (c) 2015 John Heaton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Typify/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Typify
+//
+//  Created by John Heaton on 1/23/15.
+//  Copyright (c) 2015 John Heaton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
